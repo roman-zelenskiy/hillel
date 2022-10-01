ad80408a0b8f1121bc8aa4eb51b04d424cf508f7
Roma <romazelenskiy2000@gmail.com>
2022-10-02 00:11:54 +0300

    fix homeWork9

--- homeWork9/script.js
+++ homeWork9/script.js
@@ -1,43 +1,43 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-function enterBetween() {
-    let min;
-    let max;
-    do {
-        min = prompt('enter min');
-        max = prompt('enter max');
-    } while ((isNaN(+min) || isNaN(+max)) || (min == '' || max == '') || (min > max));
-        return {
-        isBetween: (num) => {
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function enterBetween(min, max) {
+    if (min == undefined || max == undefined) {
+        do {
+            min = prompt('enter min');
+            max = prompt('enter max');
+        } while ((isNaN(+min) || isNaN(+max)) || (min == '' || max == '') || (min > max));
+    }    
+        return (num) => {
             if (num >= (+min) && num <= (+max)) {
                 return num;
             };
         }
-    };
+ 
 };
 
 
 
 
-function calculate() {
-    let inputOperation;
-    do {
-        inputOperation = prompt(`Hello!\nEnter operation\n+          -\n*          /\n pow`);
-        if (inputOperation == '+') {
-            break;
-        }
-        if (inputOperation == '-') {
-            break;
-        }
-        if (inputOperation == '*') {
-            break;
-        }
-        if (inputOperation == '/') {
-            break;
-        }
-        if (inputOperation == 'pow') {
-            break;
-        }
-    } while (true);
+function calculate(inputOperation) {
+    if (inputOperation == undefined) {
+        do {
+            inputOperation = prompt(`Hello!\nEnter operation\n+          -\n*          /\n pow`);
+            if (inputOperation == '+') {
+                break;
+            }
+            if (inputOperation == '-') {
+                break;
+            }
+            if (inputOperation == '*') {
+                break;
+            }
+            if (inputOperation == '/') {
+                break;
+            }
+            if (inputOperation == 'pow') {
+                break;
+            }
+        } while (true);
+    }
     function sum(a, b) {
         let resultSum = `Operation ${a} + ${b} finished with result ${a + b}\n`;
         return resultSum;
@@ -59,29 +59,50 @@ function calculate() {
         return resultPow;
     }
     return function (a) {
-        do {
-            a = prompt('enter a').trim();
-        } while (isNaN((+a)) || a == '');
-        a = +a;
-        return function (b) {
+        if (a == undefined) {
             do {
-                b = prompt('enter b').trim();
-            } while (isNaN(+b) || b == '');
-            b = +b;
-            if (inputOperation == '+') { 
-                alert(sum(a, b));
-            } else if (inputOperation == '-') {
-                alert(diff(a, b));
-            } else if (inputOperation == '*') {
-                alert(mult(a, b));
-            } else if (inputOperation == '/') {
-                alert(div(a, b));
-            } else if (inputOperation == 'pow') {
-                alert(pow(a, b));
-            }
+                a = prompt('enter a').trim();
+            } while (isNaN((+a)) || a == '');
+            a = +a;
+        }
+        return function (b) {
+            if (b == undefined) {
+                do {
+                    b = prompt('enter b').trim();
+                } while (isNaN(+b) || b == '');
+                b = +b;
+                if (inputOperation == '+') {
+                    alert(sum(a, b));
+                } else if (inputOperation == '-') {
+                    alert(diff(a, b));
+                } else if (inputOperation == '*') {
+                    alert(mult(a, b));
+                } else if (inputOperation == '/') {
+                    alert(div(a, b));
+                } else if (inputOperation == 'pow') {
+                    alert(pow(a, b));
+                }
+            } else {
+                if (inputOperation == '+') {
+                    alert(sum(a, b));
+                } else if (inputOperation == '-') {
+                    alert(diff(a, b));
+                } else if (inputOperation == '*') {
+                    alert(mult(a, b));
+                } else if (inputOperation == '/') {
+                    alert(div(a, b));
+                } else if (inputOperation == 'pow') {
+                    alert(pow(a, b));
+                }
+            } 
         } 
     }
 }
+let numA = 3;
+let numB = 7;
+console.log(calculate()()())
+
+
 
 
 
@@ -91,46 +112,50 @@ const products = [
   {name: 'Product 2', quantity: 3, price: 55},
   {name: 'Product 3', quantity: 22, price: 35},
 ]
-function sortByField(field) {
-    if (field == 'asc') {
-        return {
-            quantity: (a, b) => {
+
+
+function sortByField(field, sort) {
+    if (field == 'quantity') {
+        if (sort == 'asc') {
+            return (a, b) => {
                 if (a.quantity > b.quantity) {
                     return 1;
                 }
                 if (a.quantity < b.quantity) {
                     return -1;
                 }
-            },
-            price: (a, b) => {
-                if (a.price > b.price) {
-                    return 1;
-                }
-                if (a.price < b.price) {
-                    return -1;
-                }
-            }
-        }
-    } 
-    if (field == 'desc') {
-        return {
-            quantity: (a, b) => {
+            };
+        } else if (sort == 'desc') {
+            return (a, b) => {
                 if (a.quantity > b.quantity) {
                     return -1;
                 }
                 if (a.quantity < b.quantity) {
-                    return 11;
-                }
-            },
-            price: (a, b) => {
-                if (a.price > b.price) {
-                    return -1;
-                }
-                if (a.price < b.price) {
                     return 1;
                 }
-            }
+            };
         }
     }
-};
- 
+    if (field == 'price') {
+        if (sort == 'asc') {
+            return (a, b) => {
+                if (a.price > b.price) {
+                    return 1;
+                }
+                if (a.price < b.price) {
+                    return -1;
+                }
+            };
+        } else if (sort == 'desc') {
+            return (a, b) => {
+                if (a.price > b.price) {
+                    return -1;
+                }
+                if (a.price < b.price) {
+                    return 1;
+                }
+            };
+        }
+    }
+    };
+
